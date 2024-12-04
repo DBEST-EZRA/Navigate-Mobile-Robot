3a826b0702f0e511eff2edcc2f9a19548f12df3b
DBEST-EZRA <ezradbest101@gmail.com>
2024-12-04 13:34:40 +0300

    fifth commit

--- main.py
+++ main.py
@@ -13,224 +13,118 @@ p.resetSimulation()
 p.setGravity(0, 0, -10)
 useRealTimeSim = 0
 
+p.setRealTimeSimulation(useRealTimeSim)
 
-p.setRealTimeSimulation(useRealTimeSim)  # either this
-
-# load plane
+# Load plane and car
 track = p.loadURDF("data/plane/plane.urdf")
-# load car
-car = p.loadURDF("f10_racecar/racecar_differential.urdf", [0,0,0])
-# load obstacles, in this projects, we used six cube as obstacles
+car = p.loadURDF("f10_racecar/racecar_differential.urdf", [0, 0, 0])
 
+# Function to generate random obstacle positions
 def random_obstacles():
     np.random.seed()
-    xy_position = [0,0]
     xy_position_float = np.random.rand(2)
-    x_poistion_range = np.random.randint(1,10)
-    y_poistion_range = np.random.randint(1,10)
+    x_position_range = np.random.randint(1, 10)
+    y_position_range = np.random.randint(1, 10)
 
-    xy_position[0] = xy_position_float[0]+x_poistion_range
-    xy_position[1] = xy_position_float[1]+y_poistion_range
-
-    z_position = np.random.rand(1)
-    np.asarray(xy_position)
-    position = np.append(xy_position,z_position)
+    xy_position = [xy_position_float[0] + x_position_range, xy_position_float[1] + y_position_range]
+    position = np.append(xy_position, 0.2)
     return position
 
 
-# Total eight different cubes. The cube's position is changing for each time. 
+# Load obstacles
+cube_name_list = ['cube_black', 'cube_green', 'cube']
 total_cubes_number = 10
-data_path = 'data/'
-cube_list = []
-cube_name_list = ['cube_black',
-                  'cube_green',
-                  'cube']
+cube_list = [random.choice(cube_name_list) for _ in range(total_cubes_number)]
 
+# Stable obstacle positions
+p.loadURDF('data/cube_black/marble_cube.urdf', (5, 5, 0.2))
+p.loadURDF('data/cube/marble_cube.urdf', (3, 3, 0.2))
+
+# Random obstacle positions
 for i in range(total_cubes_number):
-    cube_list.append(random.choice(cube_name_list))
+    position = random_obstacles()
+    p.loadURDF(f'data/{cube_list[i]}/marble_cube.urdf', position)
 
-# Only two objects are loaded in stable positon. 
-# You need to design URDF files to load objects, otherwise the code will not excute.
-cube_stable_position_1 = p.loadURDF('data/cube_black/marble_cube.urdf',(5,5,0.2))
-cube_stable_position_2 = p.loadURDF('data/cube/marble_cube.urdf',(3,3,0.2))
-
-# 10 objects are loaed in random positions. 
-cube_1_position = random_obstacles()
-cube_1 = p.loadURDF('data/'+cube_list[0]+'/marble_cube.urdf',cube_1_position)
-
-cube_2_position = random_obstacles()
-cube_2 = p.loadURDF('data/'+cube_list[1]+'/marble_cube.urdf',cube_2_position)
-
-cube_3_position = random_obstacles()
-cube_3 = p.loadURDF('data/'+cube_list[2]+'/marble_cube.urdf',cube_3_position)
-
-cube_4_position = random_obstacles()
-cube_4 = p.loadURDF('data/'+cube_list[3]+'/marble_cube.urdf',cube_4_position)
-
-cube_5_position = random_obstacles()
-cube_5 = p.loadURDF('data/'+cube_list[4]+'/marble_cube.urdf',cube_5_position)
-
-cube_6_position = random_obstacles()
-cube_6 = p.loadURDF('data/'+cube_list[5]+'/marble_cube.urdf',cube_6_position)
-
-cube_7_position = random_obstacles()
-cube_7 = p.loadURDF('data/'+cube_list[6]+'/marble_cube.urdf',cube_7_position)
-
-cube_8_position = random_obstacles()
-cube_8 = p.loadURDF('data/'+cube_list[7]+'/marble_cube.urdf',cube_8_position)
-
-cube_9_position = random_obstacles()
-cube_9 = p.loadURDF('data/'+cube_list[8]+'/marble_cube.urdf',cube_9_position)
-
-cube_10_position = random_obstacles()
-cube_10 = p.loadURDF('data/'+cube_list[9]+'/marble_cube.urdf',cube_10_position)
-
-for wheel in range(p.getNumJoints(car)):
-    print("joint[", wheel, "]=", p.getJointInfo(car, wheel))
-    p.setJointMotorControl2(car, wheel, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
-    p.getJointInfo(car, wheel)
-
+# Initialize car joints
 wheels = [8, 15]
-print("----------------")
-
-# p.setJointMotorControl2(car,10,p.VELOCITY_CONTROL,targetVelocity=1,force=10)
-c = p.createConstraint(car, 9, car, 11, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=1, maxForce=10000)
-
-c = p.createConstraint(car, 10, car, 13, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, maxForce=10000)
-
-c = p.createConstraint(car, 9, car, 13, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, maxForce=10000)
-
-c = p.createConstraint(car, 16, car, 18, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=1, maxForce=10000)
-
-c = p.createConstraint(car, 16, car, 19, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, maxForce=10000)
-
-c = p.createConstraint(car, 17, car, 19, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, maxForce=10000)
-
-c = p.createConstraint(car, 1, car, 18, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, gearAuxLink=15, maxForce=10000)
-c = p.createConstraint(car, 3, car, 19, jointType=p.JOINT_GEAR, jointAxis=[0, 1, 0], parentFramePosition=[0, 0, 0],
-                       childFramePosition=[0, 0, 0])
-p.changeConstraint(c, gearRatio=-1, gearAuxLink=15, maxForce=10000)
-
 steering = [0, 2]
-
 hokuyo_joint = 4
 
-#
-# targetVelocitySlider = p.addUserDebugParameter("wheelVelocity", -50, 50, 0)
-# maxForceSlider = p.addUserDebugParameter("maxForce", 0, 50, 20)
-# steeringSlider = p.addUserDebugParameter("steering", -1, 1, 0)
+for wheel in range(p.getNumJoints(car)):
+    p.setJointMotorControl2(car, wheel, p.VELOCITY_CONTROL, targetVelocity=0, force=0)
 
-replaceLines = True
-# numRays = 100
+# Lidar settings
 numRays = 100
+rayLen = 8
+rayStartLen = 0.25
 rayFrom = []
 rayTo = []
 rayIds = []
 rayHitColor = [1, 0, 0]
 rayMissColor = [0, 1, 0]
-rayLen = 8
-rayStartLen = 0.25
+
 for i in range(numRays):
-    rayFrom.append([rayStartLen * math.sin(-0.5 * 0.25 * 2. * math.pi + 0.75 * 2. * math.pi * float(i) / numRays),
-                    rayStartLen * math.cos(-0.5 * 0.25 * 2. * math.pi + 0.75 * 2. * math.pi * float(i) / numRays), 0])
-    rayTo.append([rayLen * math.sin(-0.5 * 0.25 * 2. * math.pi + 0.75 * 2. * math.pi * float(i) / numRays),
-                  rayLen * math.cos(-0.5 * 0.25 * 2. * math.pi + 0.75 * 2. * math.pi * float(i) / numRays), 0])
-    if (replaceLines):
-        rayIds.append(p.addUserDebugLine(rayFrom[i], rayTo[i], rayMissColor, parentObjectUniqueId=car,
-                                         parentLinkIndex=hokuyo_joint))
-    else:
-        rayIds.append(-1)
+    angle = -0.5 * 0.25 * 2. * math.pi + 0.75 * 2. * math.pi * float(i) / numRays
+    rayFrom.append([rayStartLen * math.sin(angle), rayStartLen * math.cos(angle), 0])
+    rayTo.append([rayLen * math.sin(angle), rayLen * math.cos(angle), 0])
+    rayIds.append(p.addUserDebugLine(rayFrom[i], rayTo[i], rayMissColor, parentObjectUniqueId=car, parentLinkIndex=hokuyo_joint))
 
 
+############################################### Navigation Functions ##################################################
 
-frame = 0
-lineId = p.addUserDebugLine([0, 0, 0], [0, 0, 1], [1, 0, 0])
-lineId2 = p.addUserDebugLine([0, 0, 0], [0, 0, 1], [1, 0, 0])
-lineId3 = p.addUserDebugLine([0, 0, 0], [0, 0, 1], [1, 0, 0])
-print("lineId=", lineId)
-lastTime = time.time()
-lastControlTime = time.time()
-lastLidarTime = time.time()
+# Control the car's velocity and steering
+def control_car(car, target_velocity, steering_angle):
+    for wheel in wheels:
+        p.setJointMotorControl2(car, wheel, p.VELOCITY_CONTROL, targetVelocity=target_velocity, force=20)
+    for steer in steering:
+        p.setJointMotorControl2(car, steer, p.POSITION_CONTROL, targetPosition=steering_angle)
 
-frame = 0
-while (True):
+# Navigate the car to the target position
+def navigate_to_target(car, target_position):
+    while True:
+        # Get current car position
+        car_position, car_orientation = p.getBasePositionAndOrientation(car)
+        car_position = np.array(car_position[:2])
+        distance_to_target = np.linalg.norm(target_position - car_position)
 
-    nowTime = time.time()
-    # render Camera at 10Hertz
-    if (nowTime - lastTime > .1):
+        # Check if the car has reached the target
+        if distance_to_target < 0.5:
+            print("Target reached!")
+            break
 
-        lastTime = nowTime
+        # Lidar data processing
+        results = p.rayTestBatch(rayFrom, rayTo, parentObjectUniqueId=car, parentLinkIndex=hokuyo_joint)
+        steering_angle = 0  # Default steering
+        target_velocity = 5  # Default velocity
 
-    nowControlTime = time.time()
+        for result in results:
+            hit_fraction = result[2]
+            if hit_fraction < 0.2:  # Obstacle is close
+                steering_angle += 0.1  # Adjust to avoid obstacle
+                target_velocity = 3  # Reduce speed near obstacles
 
-    nowLidarTime = time.time()
-    # lidar at 20Hz
-    if (nowLidarTime - lastLidarTime > .3):
-        # print("Lidar!")
-        numThreads = 0
-        results = p.rayTestBatch(rayFrom, rayTo, numThreads, parentObjectUniqueId=car, parentLinkIndex=hokuyo_joint)
-        print('result [0]: ',results[0])
+        # Adjust steering angle to target
+        direction_to_target = target_position - car_position
+        target_angle = math.atan2(direction_to_target[1], direction_to_target[0])
+        steering_angle += target_angle
 
-        for i in range(numRays):
-            hitObjectUid = results[i][0]
-            hitFraction = results[i][2]
-            hitPosition = results[i][3]
-            if (hitFraction == 1.):
-                p.addUserDebugLine(rayFrom[i], rayTo[i], rayMissColor, replaceItemUniqueId=rayIds[i],
-                                   parentObjectUniqueId=car, parentLinkIndex=hokuyo_joint)
-            else:
-                localHitTo = [rayFrom[i][0] + hitFraction * (rayTo[i][0] - rayFrom[i][0]),
-                              rayFrom[i][1] + hitFraction * (rayTo[i][1] - rayFrom[i][1]),
-                              rayFrom[i][2] + hitFraction * (rayTo[i][2] - rayFrom[i][2])]
-                # print(localHitTo)
-                p.addUserDebugLine(rayFrom[i], localHitTo, rayHitColor, replaceItemUniqueId=rayIds[i],
-                                   parentObjectUniqueId=car, parentLinkIndex=hokuyo_joint)
-        lastLidarTime = nowLidarTime
+        # Control the car
+        control_car(car, target_velocity, steering_angle)
 
-    # control at 100Hz
-    if (nowControlTime - lastControlTime > .01):
-
-        # task 2 implement a function to find a path to navigate the mobile robot from start position (0,0) to the target position (11,11)
-        # current car position
-        carPos, carOrn = p.getBasePositionAndOrientation(car)
-        # get lidar information refer back to: line 159 'results' and 163-165, there are total 100 lines of lidar scans.
-        # hitObjectUid = results[i][0]
-        # hitFraction = results[i][2]
-        # hitPosition = results[i][3]
-
-        # task 1 implement a function to control the mobile robot. Input using veclocity of the wheel, and position of the steer
-        # design your codes here.
-        maxForce = 20
-        targetVelocity = 0
-        steeringAngle = 0
-
-
-
-
-
-        for wheel in wheels:
-            p.setJointMotorControl2(car, wheel, p.VELOCITY_CONTROL, targetVelocity=targetVelocity, force=maxForce)
-
-        for steer in steering:
-            p.setJointMotorControl2(car, steer, p.POSITION_CONTROL, targetPosition=-steeringAngle)
-
-
-
-        if (useRealTimeSim == 0):
-            frame += 1
+        # Step simulation
+        if not useRealTimeSim:
             p.stepSimulation()
-        lastControlTime = nowControlTime
+        time.sleep(0.01)
+
+
+############################################### Simulation Loop #######################################################
+
+# Define target position
+target_position = np.array([11, 11])
+
+# Navigate to the target
+navigate_to_target(car, target_position)
+
+# Keep simulation running after reaching the target
+while True:
+    time.sleep(1)
